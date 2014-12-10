33ac4002031d91b5e338fec6fbf3c92a6da3c370
Archit Verma <architv07@gmail.com>
2014-12-10 22:56:54 +0530

    sloving more tree problems

--- src/trees/TreeProblems.java
+++ src/trees/TreeProblems.java
@@ -1,5 +1,7 @@
 package trees;
 
+import javax.swing.text.AbstractDocument.LeafElement;
+
 public class TreeProblems {
 
 	public static void findInorderSuccessor(BTInorderSuccessor<Integer> root) {
@@ -17,11 +19,71 @@ public class TreeProblems {
 		}
 		
 		findInorderSuccessor(root.right);
-		if (root.)
+//		if (root.)
+	}
+	
+	public static void leftViewTree (BinaryTreeNode<Integer> root) {
+		
+		if (root == null) {
+			return;
+		}
+		
+		if (root.left == null && root.right == null) {
+			return;
+		}
+		System.out.println(root.data);
+		
+		leftViewTree(root.left);
+	}
+	
+	public static void rightViewTree (BinaryTreeNode<Integer> root) {
+		
+		if (root == null) {
+			return;
+		}
+		
+		rightViewTree(root.right);
+		
+		if (root.left == null && root.right == null) {
+			return;
+		}
+		System.out.println(root.data);
+		
+		
+	}
+	
+	public static void printLeafNodes(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		
+		if (root.left == null && root.right == null) {
+			System.out.println(root.data);
+		}
+		
+		printLeafNodes(root.left);
+		printLeafNodes(root.right);
+	}
+	
+	public static void boundaryView (BinaryTreeNode<Integer> root) {
+		// 1. Print the left boundary in top-down manner.
+		leftViewTree(root);
+		// 2. Print all leaf nodes from left to right, which can again be sub-divided into two sub-parts:
+		printLeafNodes(root);
+		//	2.1 Print all leaf nodes of left sub-tree from left to right.
+		// 	2.2 Print all leaf nodes of right subtree from left to right.
+		// 3. Print the right boundary in bottom-up manner.
+		rightViewTree(root);
 	}
 	
 	public static void main(String[] args) {
-
+		BinaryTreeNode<Integer> root = BinaryTree.takeIntegerInput();
+//		leftViewTree(root);
+//		rightViewTree(root);
+//		printLeafNodes(root);
+		boundaryView(root);
 	}
+	
+	
 
 }
